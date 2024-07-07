73a2823153c480cad6742aca8f39c6a6c61fbd97
John Doe <johndoe@example.com>
2024-07-06 22:50:57 -0700

    Updated styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,8 +11,10 @@ const Home = () => {
         </h1>
         <h2 id="text" className="ui center aligned container">
           Call for a personalized consultation!
-          <a href="tel:(360) 907-1509"> (360) 907-1509 </a>
         </h2>
+        <h3 id="text" className="ui center aligned container">
+          <a href="tel:(360) 907-1509"> (360) 907-1509 </a>
+        </h3>
       </div>
     </div>
   );
